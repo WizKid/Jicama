6f64a83751cd7cb227cdef7fc7ba58028c5e684b
Emil Hesslow <hesslow@gmail.com>
2010-12-14 00:20:22 +0100

    Make it possible to pretty print to easier see what happening. And started to rewrite codeparts if there are constant pool changes

--- java.py
+++ java.py
@@ -2,6 +2,14 @@ import copy
 import struct
 
 
+def indent(buf, c):
+	ret = ""
+	for line in buf.split("\n")[:-1]:
+		ret += " "*c + line +"\n"
+
+	return ret
+	
+
 class ClassError(Exception):
 	pass
 
@@ -45,6 +53,9 @@ class RefConstant(Constant):
 		self.classIndex = mapping[self.classIndex]
 		self.nameAndTypeIndex = mapping[self.nameAndTypeIndex]
 
+	def pretty(self):
+		return "%s: %d, %d\n" % (self.__class__.__name__, self.classIndex, self.nameAndTypeIndex)
+
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self.pool[self.classIndex - 1] == other.pool[other.classIndex - 1] and self.pool[self.nameAndTypeIndex - 1] == other.pool[other.nameAndTypeIndex - 1]
 
@@ -64,6 +75,9 @@ class StringConstant(Constant):
 
 	def update(self, mapping):
 		self.stringIndex = mapping[self.stringIndex]
+
+	def pretty(self):
+		return "%s: %d\n" % (self.__class__.__name__, self.stringIndex)
 
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self.pool[self.stringIndex - 1] == other.pool[other.stringIndex - 1]
@@ -106,6 +120,9 @@ class ClassConstant(Constant):
 	def update(self, mapping):
 		self.nameIndex = mapping[self.nameIndex]
 
+	def pretty(self):
+		return "%s: %d\n" % (self.__class__.__name__, self.nameIndex)
+
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self.pool[self.nameIndex - 1] == other.pool[other.nameIndex - 1]
 
@@ -126,6 +143,9 @@ class Utf8Constant(Constant):
 
 	def update(self, mapping):
 		pass
+
+	def pretty(self):
+		return "%s: %d '%s'\n" % (self.__class__.__name__, self.length, self.bytes)
 
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self.length == other.length and self.bytes == other.bytes
@@ -148,6 +168,9 @@ class NameAndTypeConstant(Constant):
 
 	def data(self):
 		return struct.pack(">cHH", self.tag, self.nameIndex, self.descriptorIndex)
+
+	def pretty(self):
+		return "%s: %d, %d\n" % (self.__class__.__name__, self.nameIndex, self.descriptorIndex)
 
 	def __eq__(self, other):
 		return self.__class__ == other.__class__ and self.pool[self.nameIndex - 1] == other.pool[other.nameIndex - 1] and self.pool[self.descriptorIndex - 1] == other.pool[other.descriptorIndex - 1]
@@ -183,6 +206,17 @@ class Field(object):
 		for attribute in self.attributes:
 			attribute.update(mapping)
 
+	def pretty(self):
+		buf = "AccessFlags: %d\n" % (self.accessFlags, )
+		buf += "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "DescriptorIndex: %d\n" % (self.descriptorIndex, )
+
+		buf += "Attributes (%d)\n" % (len(self.attributes, ))
+		for attribute in self.attributes:
+			buf += indent(attribute.pretty(), 4)
+
+		return buf
+
 
 class Method(object):
 
@@ -211,18 +245,31 @@ class Method(object):
 		for attribute in self.attributes:
 			attribute.update(mapping)
 
+	def pretty(self):
+		buf = "AccessFlags: %d\n" % (self.accessFlags, )
+		buf += "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "DescriptorIndex: %d\n" % (self.descriptorIndex, )
+
+		buf += "Attributes (%d)\n" % (len(self.attributes, ))
+		for attribute in self.attributes:
+			buf += indent(attribute.pretty(), 4)
+
+		return buf
+
 
 class Attribute(object):
 
 	@classmethod
 	def parse(cls, reader, constantPool):
 		nameIndex = reader.readU16()
-		constant = constantPool[nameIndex -1]
+		constant = constantPool[nameIndex - 1]
 
 		if isinstance(constant, Utf8Constant):
 			if constant.bytes == "Code":
 				return CodeAttribute(nameIndex, reader, constantPool)
 			if constant.bytes == "Signature":
+				return SignatureAttribute(nameIndex, reader, constantPool)
+			if constant.bytes == "SourceFile":
 				return SignatureAttribute(nameIndex, reader, constantPool)
 			if constant.bytes == "LocalVariableTable":
 				return LocalVariableTableAttribute(nameIndex, reader, constantPool)
@@ -243,12 +290,24 @@ class UnknownAttribute(Attribute):
 	def update(self, mapping):
 		self.nameIndex = mapping[self.nameIndex]
 
+	def pretty(self):
+		buf = "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "Length: %d\n" % (len(self.rawData), )
+		buf += "RawData: %s\n" % (repr(self.rawData), )
+
+		return buf
+
 
 class CodeAttribute(Attribute):
 
+	SKIP_TABLE = {
+		"\x59": 0,
+		"\x2a": 0
+	}
+
 	def __init__(self, nameIndex, reader, constantPool):
 		self.nameIndex = nameIndex
-		reader.readU32() # skip length, we will calculate it when needed
+		self.length = reader.readU32() # skip length, we will calculate it when needed
 		self.maxStack = reader.readU16()
 		self.maxLocals = reader.readU16()
 		self.code = reader.read(reader.readU32())
@@ -260,24 +319,73 @@ class CodeAttribute(Attribute):
 			self.attributes.append(Attribute.parse(reader, constantPool))
 
 	def data(self):
-		buf = struct.pack(">HHI", self.maxStack, self.maxLocals, len(self.code)) + self.code + struct.pack(">HH", self.exceptionTableLength, len(self.attributes))
+		buf = struct.pack(">HIHHI", self.nameIndex, self.length, self.maxStack, self.maxLocals, len(self.code)) + self.code + struct.pack(">HH", self.exceptionTableLength, len(self.attributes))
 		for attribute in self.attributes:
 			buf += attribute.data()
 
-		return struct.pack(">HI", self.nameIndex, len(buf)) + buf
+		return buf
+
+	def updateCode(self, code, mapping):
+		newCode = ""
+
+		length = len(code)
+		i = 0
+		while i < length:
+			skip = CodeAttribute.SKIP_TABLE.get(code[i])
+			if not skip is None:
+				newCode += code[i:i + skip + 1]
+				i += skip + 1
+				continue
+
+			if code[i] == "\x12":
+				index = struct.unpack(">B", code[i+1:i+2])[0]
+				index = mapping[index]
+				newCode += code[i] + struct.pack(">B", index)
+				i += 2
+				continue
+
+			if code[i] == "\xb2" or code[i] == "\xb4" or code[i] == "\xb6" or code[i] == "\xb7" or code[i] == "\xbb":
+				index = struct.unpack(">H", code[i+1:i+3])[0]
+				index = mapping[index]
+				newCode += code[i] + struct.pack(">H", index)
+				i += 3
+				continue
+
+			break
+
+		if len(newCode) < len(code):
+			newCode += code[len(newCode):]
+
+		return newCode
 
 	def update(self, mapping):
 		self.nameIndex = mapping[self.nameIndex]
 
+		self.code = self.updateCode(self.code, mapping)
+
 		for attribute in self.attributes:
 			attribute.update(mapping)
+
+	def pretty(self):
+		buf = "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "Length: %d\n" % (self.length, )
+		buf += "MaxStack: %d\n" % (self.maxStack, )
+		buf += "MaxLocals: %d\n" % (self.maxLocals, )
+		buf += "Code: %s\n" % (repr(self.code), )
+		buf += "ExceptionTableLength: %d\n" % (self.exceptionTableLength, )
+
+		buf += "Attributes (%d)\n" % (len(self.attributes), )
+		for attribute in self.attributes:
+			buf += indent(attribute.pretty(), 4)
+
+		return buf
 
 
 class LocalVariableTableAttribute(Attribute):
 
 	def __init__(self, nameIndex, reader, constantPool):
 		self.nameIndex = nameIndex
-		reader.readU32() # skip length, we will calculate it when needed
+		self.length = reader.readU32() # skip length, we will calculate it when needed
 
 		length = reader.readU16()
 		self.localVariables = []
@@ -285,16 +393,26 @@ class LocalVariableTableAttribute(Attribute):
 			self.localVariables.append(LocalVariable(reader))
 
 	def data(self):
-		buf = struct.pack(">H", len(self.localVariables))
+		buf = struct.pack(">HIH", self.nameIndex, self.length, len(self.localVariables))
 		for localVariable in self.localVariables:
 			buf += localVariable.data()
 
-		return struct.pack(">HI", self.nameIndex, len(buf)) + buf
+		return buf
 
 	def update(self, mapping):
 		self.nameIndex = mapping[self.nameIndex]
 		for localVariable in self.localVariables:
 			localVariable.update(mapping)
+
+	def pretty(self):
+		buf = "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "Length: %d\n" % (self.length, )
+
+		buf += "LocalVariables (%d)\n" % (len(self.localVariables), )
+		for localVariable in self.localVariables:
+			buf += indent(localVariable.pretty(), 4)
+
+		return buf
 
 
 class SignatureAttribute(Attribute):
@@ -310,6 +428,35 @@ class SignatureAttribute(Attribute):
 	def update(self, mapping):
 		self.nameIndex = mapping[self.nameIndex]
 		self.signatureIndex = mapping[self.signatureIndex]
+
+	def pretty(self):
+		buf = "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "Length: %d\n" % (2, )
+		buf += "SignatureIndex: %d\n" % (self.signatureIndex, )
+
+		return buf
+
+
+class SourceFileAttribute(Attribute):
+
+	def __init__(self, nameIndex, reader, constantPool):
+		self.nameIndex = nameIndex
+		reader.readU32() # skip length, we will calculate it when needed
+		self.sourceFileIndex = reader.readU16()
+
+	def data(self):
+		return struct.pack(">HIH", self.nameIndex, 2, self.sourceFileIndex)
+
+	def update(self, mapping):
+		self.nameIndex = mapping[self.nameIndex]
+		self.sourceFileIndex = mapping[self.sourceFileIndex]
+
+	def pretty(self):
+		buf = "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "Length: %d\n" % (2, )
+		buf += "SourceFileIndex: %d\n" % (self.sourceFileIndex, )
+
+		return buf
 
 
 class LocalVariable(object):
@@ -328,7 +475,14 @@ class LocalVariable(object):
 		self.nameIndex = mapping[self.nameIndex]
 		self.descriptorIndex = mapping[self.descriptorIndex]
 
+	def pretty(self):
+		buf = "StartPC: %d\n" % (self.startPc, )
+		buf += "Length: %d\n" % (self.length, )
+		buf += "NameIndex: %d\n" % (self.nameIndex, )
+		buf += "DescriptorIndex: %d\n" % (self.descriptorIndex, )
+		buf += "Index: %d\n" % (self.index, )
 
+		return buf
 
 class Class(object):
 
@@ -344,6 +498,42 @@ class Class(object):
 
 	def __init__(self, path):
 		self.path = path
+
+	def pretty(self):
+		buf = "Magic: "+ repr("\xCA\xFE\xBA\xBE") +"\n"
+		buf += "Version: %d.%d\n" % (self.version[1] , self.version[0])
+		buf += "ConstantPoolLength: %d\n" % (len(self.constantPool), )
+
+		# Constant Pool
+		buf += "ConstantPool (%d)\n" % (len(self.constantPool, ))
+		for constant in self.constantPool:
+			buf += indent(constant.pretty(), 4)
+
+		buf += "AccessFlags: %d\n" % (self.accessFlags, )
+		buf += "ThisClass: %d\n" % (self.thisClass, )
+		buf += "SuperClass: %d\n" % (self.superClass, )
+
+		# Interfaces
+		buf += "Interfaces (%d)\n" % (len(self.interfaces, ))
+		for interface in self.interfaces:
+			buf += indent(interface.pretty(), 4)
+
+		# Fields
+		buf += "Fields (%d)\n" % (len(self.fields, ))
+		for field in self.fields:
+			buf += indent(field.pretty(), 4)
+
+		# Methods
+		buf += "Methods (%d)\n" % (len(self.methods, ))
+		for method in self.methods:
+			buf += indent(method.pretty(), 4)
+
+		# Attributes
+		buf += "Attributes (%d)\n" % (len(self.attributes, ))
+		for attribute in self.attributes:
+			buf += indent(attribute.pretty(), 4)
+
+		return buf
 
 	def diff(self, other):
 		# Should really found the minimum number of moves that needs to be done
@@ -376,8 +566,6 @@ class Class(object):
 
 		with open('patch', 'wb') as fp:
 			fp.write(patch)
-
-		print mapping
 
 		for constant in self.constantPool:
 			constant.update(mapping)
--- patch.py
+++ patch.py
@@ -3,13 +3,22 @@ import java
 org = java.Class("PerFieldAnalyzerWrapper-3.0.2.class")
 org.parse()
 
+# print org.pretty()
+
+# The new method is 122 bytes not including constant pool changes
+
 reference = java.Class("PerFieldAnalyzerWrapper-3.0.3.class")
 reference.parse()
+
+#print reference.pretty()
 
 #with open("PerFieldAnalyzerWrapper-3.0.3.class.regenerate", "wb") as fp:
 #	fp.write(reference.data())
 
 org.diff(reference)
 
+print org.pretty()
+
 with open("PerFieldAnalyzerWrapper-3.0.2.regenerate.class", "wb") as fp:
 	fp.write(org.data())
+
